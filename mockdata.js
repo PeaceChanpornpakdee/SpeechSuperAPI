879cae234ad085dcd60d99bb04a4355f7366124d
Krit <yothinpeace@gmail.com>
2023-11-22 12:59:01 +0700

    ENH: Mock json result

---
+++ mockdata.js
@@ -0,0 +1,344 @@
+var mockResult = {
+    "dtLastResponse": "2023-11-22 13:55:49:198",
+    "recordId": "655d97e47a39bc1f00012efb",
+    "params": {
+        "app": {
+            "applicationId": "1700030832000223",
+            "sig": "656c35bdfbcee6247642c62e9ad4ec5f64de7672",
+            "userId": "guest",
+            "timestamp": "1700632544254"
+        },
+        "audio": {
+            "sampleRate": 16000,
+            "audioType": "wav",
+            "channel": 1,
+            "sampleBytes": 2
+        },
+        "request": {
+            "coreType": "sent.eval",
+            "refText": "Test One Two Three",
+            "tokenId": "8652D677BC074F5994E5B6743A9C5DE8"
+        }
+    },
+    "result": {
+        "pronunciation": 81,
+        "pause_count": 0,
+        "rear_tone": "fall",
+        "fluency": 96,
+        "speed": 93,
+        "rhythm": 92,
+        "integrity": 100,
+        "resource_version": "3.7.1",
+        "duration": "3.904",
+        "warning": [
+            {
+                "code": 1002,
+                "message": "Audio volume too low!"
+            }
+        ],
+        "kernel_version": "6.1.0",
+        "words": [
+            {
+                "word": "Test",
+                "span": {
+                    "end": 161,
+                    "start": 95
+                },
+                "scores": {
+                    "pronunciation": 80,
+                    "overall": 80,
+                    "prominence": 0
+                },
+                "word_parts": [
+                    {
+                        "beginIndex": 0,
+                        "part": "Test",
+                        "charType": 0,
+                        "endIndex": 3
+                    }
+                ],
+                "pause": {
+                    "type": 0,
+                    "duration": 0
+                },
+                "phonics": [
+                    {
+                        "spell": "T",
+                        "phoneme": [
+                            "t"
+                        ],
+                        "overall": 99
+                    },
+                    {
+                        "spell": "e",
+                        "phoneme": [
+                            "ɛ"
+                        ],
+                        "overall": 76
+                    },
+                    {
+                        "spell": "s",
+                        "phoneme": [
+                            "s"
+                        ],
+                        "overall": 79
+                    },
+                    {
+                        "spell": "t",
+                        "phoneme": [
+                            "t"
+                        ],
+                        "overall": 70
+                    }
+                ],
+                "charType": 0,
+                "phonemes": [
+                    {
+                        "pronunciation": 99,
+                        "span": {
+                            "end": 107,
+                            "start": 95
+                        },
+                        "phoneme": "t"
+                    },
+                    {
+                        "pronunciation": 76,
+                        "span": {
+                            "end": 131,
+                            "start": 107
+                        },
+                        "phoneme": "ɛ"
+                    },
+                    {
+                        "pronunciation": 79,
+                        "span": {
+                            "end": 151,
+                            "start": 131
+                        },
+                        "phoneme": "s"
+                    },
+                    {
+                        "pronunciation": 70,
+                        "span": {
+                            "end": 161,
+                            "start": 151
+                        },
+                        "phoneme": "t"
+                    }
+                ]
+            },
+            {
+                "word": "One",
+                "span": {
+                    "end": 228,
+                    "start": 164
+                },
+                "scores": {
+                    "pronunciation": 98,
+                    "overall": 98,
+                    "prominence": 0
+                },
+                "word_parts": [
+                    {
+                        "beginIndex": 5,
+                        "part": "One",
+                        "charType": 0,
+                        "endIndex": 7
+                    }
+                ],
+                "pause": {
+                    "type": 0,
+                    "duration": 0
+                },
+                "phonics": [
+                    {
+                        "spell": "O",
+                        "phoneme": [
+                            "w"
+                        ],
+                        "overall": 98
+                    },
+                    {
+                        "spell": "n",
+                        "phoneme": [
+                            "ʌ"
+                        ],
+                        "overall": 100
+                    },
+                    {
+                        "spell": "e",
+                        "phoneme": [
+                            "n"
+                        ],
+                        "overall": 94
+                    }
+                ],
+                "charType": 0,
+                "phonemes": [
+                    {
+                        "pronunciation": 98,
+                        "span": {
+                            "end": 180,
+                            "start": 164
+                        },
+                        "phoneme": "w"
+                    },
+                    {
+                        "pronunciation": 100,
+                        "span": {
+                            "end": 199,
+                            "start": 180
+                        },
+                        "phoneme": "ʌ"
+                    },
+                    {
+                        "pronunciation": 94,
+                        "span": {
+                            "end": 228,
+                            "start": 199
+                        },
+                        "phoneme": "n"
+                    }
+                ]
+            },
+            {
+                "word": "Two",
+                "span": {
+                    "end": 292,
+                    "start": 231
+                },
+                "scores": {
+                    "pronunciation": 90,
+                    "overall": 90,
+                    "prominence": 0
+                },
+                "word_parts": [
+                    {
+                        "beginIndex": 9,
+                        "part": "Two",
+                        "charType": 0,
+                        "endIndex": 11
+                    }
+                ],
+                "pause": {
+                    "type": 0,
+                    "duration": 0
+                },
+                "phonics": [
+                    {
+                        "spell": "T",
+                        "phoneme": [
+                            "t"
+                        ],
+                        "overall": 98
+                    },
+                    {
+                        "spell": "wo",
+                        "phoneme": [
+                            "u"
+                        ],
+                        "overall": 83
+                    }
+                ],
+                "charType": 0,
+                "phonemes": [
+                    {
+                        "pronunciation": 98,
+                        "span": {
+                            "end": 248,
+                            "start": 231
+                        },
+                        "phoneme": "t"
+                    },
+                    {
+                        "pronunciation": 83,
+                        "span": {
+                            "end": 292,
+                            "start": 248
+                        },
+                        "phoneme": "u"
+                    }
+                ]
+            },
+            {
+                "word": "Three",
+                "span": {
+                    "end": 351,
+                    "start": 296
+                },
+                "scores": {
+                    "pronunciation": 55,
+                    "overall": 55,
+                    "prominence": 0
+                },
+                "word_parts": [
+                    {
+                        "beginIndex": 13,
+                        "part": "Three",
+                        "charType": 0,
+                        "endIndex": 17
+                    }
+                ],
+                "pause": {
+                    "type": 0,
+                    "duration": 0
+                },
+                "phonics": [
+                    {
+                        "spell": "Th",
+                        "phoneme": [
+                            "θ"
+                        ],
+                        "overall": 0
+                    },
+                    {
+                        "spell": "r",
+                        "phoneme": [
+                            "r"
+                        ],
+                        "overall": 1
+                    },
+                    {
+                        "spell": "ee",
+                        "phoneme": [
+                            "i"
+                        ],
+                        "overall": 96
+                    }
+                ],
+                "charType": 0,
+                "phonemes": [
+                    {
+                        "pronunciation": 0,
+                        "span": {
+                            "end": 307,
+                            "start": 296
+                        },
+                        "phoneme": "θ"
+                    },
+                    {
+                        "pronunciation": 1,
+                        "span": {
+                            "end": 312,
+                            "start": 307
+                        },
+                        "phoneme": "r"
+                    },
+                    {
+                        "pronunciation": 96,
+                        "span": {
+                            "end": 351,
+                            "start": 312
+                        },
+                        "phoneme": "i"
+                    }
+                ]
+            }
+        ],
+        "overall": 83
+    },
+    "applicationId": "1700030832000223",
+    "refText": "Test One Two Three",
+    "tokenId": "8652D677BC074F5994E5B6743A9C5DE8",
+    "eof": 1
+}
